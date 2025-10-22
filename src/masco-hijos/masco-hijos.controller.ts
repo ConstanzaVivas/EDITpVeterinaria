b8ced3d126854891539023333271c82b0d788ad7
unknown <constanzad.vivas@gmail.com>
2025-10-22 11:49:50 -0300

    Get y post

--- src/masco-hijos/masco-hijos.controller.ts
+++ src/masco-hijos/masco-hijos.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { MascoHijosService } from './masco-hijos.service';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('masco-hijos')
-export class MascoHijosController {}
+export class MascoHijosController {
+       constructor(private readonly mascotasService: MascoHijosService){}
+     @Get('pacientes')
+        getMascotas(): string {
+         return this.mascotasService.getMascotas();
+     }
+     @Get('pacientes/:id')
+        getMascotasId(@Param('id') id:String) : any{ 
+            
+
+            return this.mascotasService.getMacotasId(id);
+        }
+    @Post('ingresar')
+    ingresarMascota(@Body() mascota:any)  {
+
+        return this.mascotasService.ingreseMascota(mascota);
+    }
+}
--- src/masco-hijos/masco-hijos.service.ts
+++ src/masco-hijos/masco-hijos.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MascoHijosService {}
+export class MascoHijosService {
+
+ mascotas: any = [];
+    constructor(){
+        let mascota ={
+            "id" : "1",
+            "nombre" : "Pepe",
+
+            "especie" : "Perro",
+            "raza":"bulldog",
+            "edad" : "1",
+            "duenio" : "Rodolfo Fernandez",
+            "telefono" : "0303456"
+        };
+        this.mascotas.push(mascota);
+        mascota ={
+            "id" : "2",
+            "nombre" : "Branca",
+            "especie" : "Gato",
+            "raza":"british",
+            "edad" : "2",
+            "duenio" : "Ana Loise",
+            "telefono" : "0303457"
+        };
+        this.mascotas.push(mascota);
+        mascota ={
+            "id" : "3",
+            "nombre" : "Beto",
+            "especie" : "gato",
+            "raza":"esfinge",
+            "edad" : "14",
+            "duenio" : "Carlos Lop",  
+            "telefono" : "0303458"
+        };
+        this.mascotas.push(mascota);
+         }
+    getMascotas(){
+        return this.mascotas;
+    }
+    getMacotasId(id:String){
+        for(let i = 0; i < this.mascotas.length; i++){
+            if (this.mascotas[i].id == id){
+                return this.mascotas[i];
+            }  
+        } 
+        
+        return "No se encontran esos datos intente nuevamente";
+    }
+
+ ingreseMascota(mascota:any){
+        if (mascota === null || mascota === undefined){
+            return  "Datos de mascota invalidos";
+
+        }
+        this.mascotas.push(mascota);
+        return "Mascota ingresada correctamente";
+        
+
+    }}
+    
+    
+            
